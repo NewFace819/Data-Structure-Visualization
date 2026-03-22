dec4c9e8b573308a3ed3385c7c2dc5c8ad0dec89
minh do <nhanminhasian@gmail.com>
2026-03-22 19:36:01 +0700

    Add constructor and fix typo

--- src/Heap/heap.h
+++ src/Heap/heap.h
@@ -2,29 +2,40 @@
 #define Heap_h
 
 #include <vector>
-#include <string>
 
 struct HeapStep
 {
 	std::vector<int> currentHeap;
 	int highlightedNode1;
 	int highlightedNode2;
-	int lindID;
+	int lineID;
+
+	HeapStep(const std::vector<int>& heap, int node1 = -1, int node2 = -1, int line = -1)
+		: currentHeap(heap), highlightedNode1(node1), highlightedNode2(node2), lineID(line) {}
 };
 
-struct VisualHeap 
+class VisualHeap 
 {
+private:
 	std::vector<HeapStep> steps;
 	std::vector<int> heap;
+	void siftUp(int index);
+	void siftDown(int index);
+	void recordStep(int node1 = -1, int node2 = -1, int line = -1) {
+		steps.emplace_back(heap, node1, node2, line);
+	}
+public:
 	void init(const std::vector<int>& data);
 	void push(int value);
 	void pop();
-	void shiftUp(int index);
-	void shiftDown(int index);
-
-	void clear() {
+	void update(int index, int newValue);
+	void search(int value);
+	const std::vector<HeapStep>& getSteps() const {
+		return steps;
+	}
+	void clearSteps() {
 		steps.clear();
 		heap.clear();
 	}
 };
-#endif // !Heap_h
+#endif // Heap_h
